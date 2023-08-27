5f5ab1010760339f5f5f1309abc4118fd6551ba6
13Rai <133304819+13Rai@users.noreply.github.com>
2023-08-27 09:11:23 +0545

    Part 1d continued

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const Button = ({ text, onClick }) => {
+  return (
+    <div>
+      <button onClick={onClick}>{text}</button>
+    </div>
+  );
+};
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ];
+
+  const initialAnecdoteVotes = new Array(anecdotes.length).fill(0);
+  const [selected, setSelected] = useState(0);
+  const [anecdoteVotes, setAnecdoteVotes] = useState(initialAnecdoteVotes);
+
+  const handleAnecdoteClick = () => {
+    const random = Math.floor(Math.random() * anecdotes.length);
+    setSelected(random);
+  };
+
+  const handleVoting = () => {
+    const newVotes = [...anecdoteVotes];
+    newVotes[selected] += 1;
+    setAnecdoteVotes(newVotes);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Anecdote of the Day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>Number of votes: {anecdoteVotes[selected]}</p>
+      <Button onClick={handleAnecdoteClick} text="Next Anecdote" />
+      <Button onClick={handleVoting} text="Vote" />
+    </div>
+  );
+};
 
-export default App
+export default App;
